d2e08910af4abde4bc6846404adc3301ee8073e9
Colty1999 <mateuszgietka99@gmail.com>
2023-08-17 16:19:13 +0200

    Home widget added; Google fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp());
 
@@ -6,20 +7,42 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: const Text('Hello world'),
+          title: const Text('Flutter Clock'),
           centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
-        body: const Center(
-          child: Text('Hello world'),
+        body: Center(
+          child: Text(
+            'Hello world',
+            style: GoogleFonts.lato(
+              textStyle: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey[600],
+                // fontFamily:
+              ),
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
+          backgroundColor: Colors.red[600],
           child: const Icon(Icons.add),
         ),
-      ),
-    );
+      );
   }
 }
